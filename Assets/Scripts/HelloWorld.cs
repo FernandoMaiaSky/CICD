162f5379e2c2d5e399a1d5732b9d5c914575d7c0
Fernando Maia <fernando.maia@sky.uk>
2023-11-02 20:15:22 +0000

    TMP esstentials

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -1,9 +1,13 @@
+using TMPro;
 using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI text;
+
     void Start()
     {
+        text.text = "Hello World";
         Debug.Log("Hello World!!! What a beautiful day");
     }
 }
